b72af85ea1e51109740307d928709bedfd578295
KrishKorat <krishkorat33@gmail.com>
2025-12-05 21:05:36 +0530

    Image Slider created

---
+++ 04-Image-Slider/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ImageSlider from './ImageSlider'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <ImageSlider url={'https://picsum.photos/v2/list'} page={'1'} limit={'10'}/>
+    </>
+  )
+}
+
+export default App
